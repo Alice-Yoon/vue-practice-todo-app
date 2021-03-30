7ef271553cd85c1ed87779f7928ace3f787d777f
윤지혜 <yoonjihye@yunjihyeui-MacBook-Pro.local>
2021-03-30 21:48:15 +0900

    todo page completed

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app id="inspire">
-    <v-navigation-drawer v-model="drawer" app>
+    <v-navigation-drawer permanent v-if="drawer" app>
       <v-list-item>
         <v-list-item-content>
           <v-list-item-title class="title">
@@ -21,6 +21,7 @@
         <v-list-item
           v-for="item in items"
           :key="item.title"
+          :to="item.to"
           link
         >
           <v-list-item-icon>
@@ -34,14 +35,41 @@
       </v-list>
     </v-navigation-drawer>
 
-    <v-app-bar app>
+    <v-app-bar
+      app
+      color="#fcb69f"
+      dark
+      src="banner.jpg"
+      prominent
+    >
+      <template v-slot:img="{ props }">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(19,84,122,.5), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
 
-      <v-toolbar-title>Application</v-toolbar-title>
+      <v-app-bar-title>Todo</v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <router-view></router-view>
     </v-main>
   </v-app>
 </template>
@@ -51,9 +79,8 @@
     data: () => ({ 
       drawer: null,
       items: [
-        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
-        { title: 'Photos', icon: 'mdi-image' },
-        { title: 'About', icon: 'mdi-help-box' },
+        { title: 'Todo', icon: 'mdi-format-list-checks', to: '/' },
+        { title: 'About', icon: 'mdi-help-box', to: '/about' },
       ], 
     }),
   }
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,9 @@
 <template>
-  <div class="about">
+  <div class="about pa-6">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non magni quaerat accusantium dignissimos quia architecto obcaecati deserunt! Magni in obcaecati, ipsum dolore saepe cum nostrum est veritatis a mollitia!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non magni quaerat accusantium dignissimos quia architecto obcaecati deserunt! Magni in obcaecati, ipsum dolore saepe cum nostrum est veritatis a mollitia!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non magni quaerat accusantium dignissimos quia architecto obcaecati deserunt! Magni in obcaecati, ipsum dolore saepe cum nostrum est veritatis a mollitia!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur non magni quaerat accusantium dignissimos quia architecto obcaecati deserunt! Magni in obcaecati, ipsum dolore saepe cum nostrum est veritatis a mollitia!</p>
   </div>
 </template>
